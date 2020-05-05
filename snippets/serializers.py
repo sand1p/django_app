f50233ece61a058778792b0fcd932f4c4551f3e0
Sandip Gatkal <sandip@ridecell.com>
2020-05-05 16:08:08 +0530

    Refactor serialzer to user ModelSerialzer instead of Serializer

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,36 +3,19 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
+"""
+Test the abstraction using following methods, 
+from snippets.serializers import SnippetSerializer
+serializer = SnippetSerializer()
+print(repr(serializer))
 
-class SnippetSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    code = serializers.CharField(style={'base_template': 'text_area.html'})
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python', )
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+"""
 
-    def create(self, validated_data):
-        """
-        create and return a new Snippet given a validated data
-        :param validated_data:
-        :return:
-        """
 
-        return Snippet.objects.create(**validated_data)
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
 
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing 'Snippet' instance, given the validated data.
-        :param instance:
-        :param validated_data:
-        :return:
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.language = validated_data.get('language', instance.language)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
+
 
